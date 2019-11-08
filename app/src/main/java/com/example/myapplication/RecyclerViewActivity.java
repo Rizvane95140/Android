d84fa13c77a0869aa35b25e54398a75626c745d5
Rizvane <rizvane95140@gmail.com>
2019-11-08 14:15:30 +0100

    logement

--- app/src/main/java/com/example/myapplication/RecyclerViewActivity.java
+++ app/src/main/java/com/example/myapplication/RecyclerViewActivity.java
@@ -1,6 +1,7 @@
 package com.example.myapplication;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -10,9 +11,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CalendarView;
 import android.widget.TextView;
 
 import com.example.myapplication.Entities.Logement;
+
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -106,8 +110,29 @@ public class RecyclerViewActivity extends AppCompatActivity {
         return logementList;
     }
 
-    public void openDialog()
+    public void openDialog(Logement logement)
     {
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
 
+        LayoutInflater inflater = this.getLayoutInflater();
+        View dialogView = inflater.inflate(R.layout.dialog_reservation, null);
+        dialogBuilder.setView(dialogView);
+
+        TextView txtReservation = dialogView.findViewById(R.id.txtReservation);
+        String text = txtReservation.getText().toString();
+        txtReservation.setText(text + logement.getTitre());
+
+        final CalendarView calendarView = dialogView.findViewById(R.id.btnRerserver);
+
+        Button btnReservation = dialogView.findViewById(R.id.btnRerserver);
+
+        btnReservation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Long date = calendarView.getDate();
+
+
+            }
+        });
     }
 }
